352b1ebaf10798e08ed5fa9c54a385f174e8ed84
Hal <hal@hotmail.com>
2024-04-26 20:58:30 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    alert('Organize suas task list!')
+
+    $('#taskForm').submit(function(event) {
+        event.preventDefault(); 
+
+        var taskName = $('#taskInput').val();
+        if (taskName.trim() !== '') {
+            $('#taskList').append('<li>' + taskName + '</li>');
+            $('#taskInput').val('');
+        }
+    });
+
+    $('#taskList').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
+});
